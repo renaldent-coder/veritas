8409e6e6c8b93a0a091bcaaae9f53cc2da7d67df
renaldent coder <renaldent@gmail.com>
2026-08-14 01:11:41 +0100

    OO

--- backend/veritas_backend/urls.py
+++ backend/veritas_backend/urls.py
@@ -1,12 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse("Veritas Asset Recovery API is running!")
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', home),  # 👈 Add this
     path('admin/', admin.site.urls),
     path('api/', include('apps.api.urls')),
 ]
