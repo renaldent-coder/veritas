3940364e9d5194c169c799bf3bcfca2a48cb04a9
renaldent coder <renaldent@gmail.com>
2026-08-14 02:04:24 +0100

    Added home view and verified API routes

--- backend/veritas_backend/urls.py
+++ backend/veritas_backend/urls.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
 from django.conf import settings
 from django.conf.urls.static import static
 
+# 👇 Add this home view
+def home(request):
+    return HttpResponse("Veritas Asset Recovery API is running!")
+
 urlpatterns = [
+    path('', home),  # 👈 Add this line
     path('admin/', admin.site.urls),
     path('api/', include('apps.api.urls')),
 ]
