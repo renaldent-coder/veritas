f7df22a5374f44af66ff674f223b38b74150ad28
renaldent coder <renaldent@gmail.com>
2026-08-14 11:46:05 +0100

    Simplified admin to minimum for debugging

--- backend/apps/accounts/admin.py
+++ backend/apps/accounts/admin.py
@@ -1,65 +1,8 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from django.utils.html import format_html
-from django.urls import reverse
 from .models import Client
 
-
 @admin.register(Client)
-class ClientAdmin(UserAdmin):
-    list_display = [
-        'email',
-        'full_name',
-        'country',
-        'date_joined',
-        'is_active',
-        'email_verified',
-        # 'case_count',  # Temporarily removed
-    ]
-    
-    list_filter = [
-        'is_active',
-        'email_verified',
-        'country',
-        'date_joined',
-    ]
-    
-    search_fields = [
-        'email',
-        'first_name',
-        'last_name',
-        'username',
-        'phone_number',
-    ]
-    
-    readonly_fields = [
-        'id',
-        'date_joined',
-        'last_active',
-    ]
-    
-    fieldsets = (
-        ('Personal Information', {
-            'fields': ('username', 'first_name', 'last_name', 'email', 'phone_number', 'country', 'telegram_handle')
-        }),
-        ('Account Status', {
-            'fields': ('is_active', 'is_staff', 'is_superuser', 'email_verified', 'date_joined', 'last_active')
-        }),
-        ('Legal Agreements', {
-            'fields': ('agreed_to_terms', 'agreed_to_nda', 'agreed_to_fee_structure')
-        }),
-        ('Permissions', {
-            'fields': ('groups', 'user_permissions'),
-            'classes': ('collapse',)
-        }),
-    )
-    
-    add_fieldsets = (
-        ('Create Client', {
-            'fields': ('username', 'email', 'first_name', 'last_name', 'password1', 'password2')
-        }),
-    )
-    
-    def full_name(self, obj):
-        return obj.get_full_name()
-    full_name.short_description = 'Full Name'
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ['id', 'email', 'first_name', 'last_name', 'is_active']
+    search_fields = ['email', 'first_name', 'last_name']
+    list_filter = ['is_active']
--- backend/apps/cases/admin.py
+++ backend/apps/cases/admin.py
@@ -1,65 +1,24 @@
 from django.contrib import admin
-from django.utils.html import format_html
-from django.urls import reverse
 from .models import Case, Document, InternalNote, ClientCommunication, AuditLog
-
 
 @admin.register(Case)
 class CaseAdmin(admin.ModelAdmin):
-    list_display = [
-        'case_number',
-        'client_link',
-        'loss_amount_display',
-        'status_colored',
-        'scam_category',
-        'submitted_at',
-        # 'assigned_agent',  # Temporarily removed
-    ]
-    
-    list_filter = [
-        'status',
-        'scam_category',
-        'transaction_method',
-        'submitted_at',
-    ]
-    
-    search_fields = [
-        'case_number',
-        'client__email',
-        'client__first_name',
-        'client__last_name',
-        'narrative',
-    ]
-    
-    readonly_fields = [
-        'case_number',
-        'submitted_at',
-        'updated_at',
-    ]
-    
-    def client_link(self, obj):
-        url = reverse('admin:accounts_client_change', args=[obj.client.id])
-        return format_html('<a href="{}" style="color: #D4AF37;">{}</a>', url, obj.client.get_full_name())
-    client_link.short_description = 'Client'
-    
-    def loss_amount_display(self, obj):
-        return format_html('<strong>${:,.2f}</strong>', obj.loss_amount)
-    loss_amount_display.short_description = 'Loss Amount'
-    
-    def status_colored(self, obj):
-        colors = {
-            'PENDING_REVIEW': '#FFA500',
-            'UNDER_INVESTIGATION': '#1E90FF',
-            'EXCHANGE_CONTACTED': '#9370DB',
-            'RECOVERY_IN_PROGRESS': '#FF6B6B',
-            'RECOVERED': '#2ECC71',
-            'CLOSED': '#95A5A6',
-            'UNRECOVERABLE': '#E74C3C',
-        }
-        color = colors.get(obj.status, '#FFFFFF')
-        return format_html(
-            '<span style="background-color: {}; color: #0A1628; padding: 4px 12px; border-radius: 4px; font-weight: 600; font-size: 12px;">{}</span>',
-            color,
-            obj.get_status_display()
-        )
-    status_colored.short_description = 'Status'
+    list_display = ['case_number', 'client', 'loss_amount', 'status', 'submitted_at']
+    search_fields = ['case_number', 'client__email']
+    list_filter = ['status']
+
+@admin.register(Document)
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ['file_name', 'case', 'uploaded_at']
+
+@admin.register(InternalNote)
+class InternalNoteAdmin(admin.ModelAdmin):
+    list_display = ['case', 'author', 'created_at']
+
+@admin.register(ClientCommunication)
+class ClientCommunicationAdmin(admin.ModelAdmin):
+    list_display = ['case', 'subject', 'sent_at']
+
+@admin.register(AuditLog)
+class AuditLogAdmin(admin.ModelAdmin):
+    list_display = ['case', 'user', 'new_status', 'timestamp']
